c5552bea4d294014e66618885a3f473e58b18e0c
danil179 <32551661+danil179@users.noreply.github.com>
2018-08-12 23:02:39 +0300

    Added client packet API Server API is WIP

---
+++ GameServerLib/Logic/API/Network/IServerAPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// FIXME: define these as common classes, should be added with the full integration
+using NetID = System.UInt32;
+using ClientID = System.UInt32;
+using Waypoints = System.UInt32; // wrong, only for compilation
+using Vector2D = System.UInt32; // wrong, only for compilation
+using Vector3D = System.UInt32; // wrong, only for compilation
+using TipCommand = System.UInt32; // wrong, only for compilation
+using TipConfig = System.UInt32; // wrong, only for compilation
+using ChatType = System.UInt32; // wrong, only for compilation
+using PingType = System.UInt32; // wrong, only for compilation
+using RespawnPointEvent = System.UInt32; // wrong, only for compilation
+using QuestEvent = System.UInt32; // wrong, only for compilation
+using VOFlags = System.UInt32; // wrong, only for compilation
+using DrawPathNodeType = System.UInt32; // wrong, only for compilation
+
+namespace LeagueSandbox.GameServer.Logic.API.Network
+{
+    public interface IServerAPI
+    {
+        // WIP
+    }
+}
